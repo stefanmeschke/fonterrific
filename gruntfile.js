2bd919c2701e585bd505ed2a3f9a8d4412bfd2a0
Stefan Meschke <stefan.meschke@gmail.com>
2016-10-30 03:06:44 +0100

    init gruntjs

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+var fontName = 'fonterrific';
+var fontClass = 'ft';
+var fontClassPrefix = 'ft-';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        webfont: {
+            icons: {
+                src: 'icons/svg/*.svg',
+                dest: 'build/font',
+                destCss: 'build/',
+                options: {
+                    fontBaseName: fontName,
+                    fontFilename: fontName,
+                    template: 'templates/fonterrific.css',
+                    htmlDemoTemplate: 'templates/fonterrific.html',
+                    templateOptions: {
+                        fontBaseName: fontName,
+                        baseClass: fontClass,
+                        classPrefix: fontClassPrefix
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-webfont');
+
+    grunt.registerTask('default', ['webfont']);
+};
